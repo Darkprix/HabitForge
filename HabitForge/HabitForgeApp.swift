16fc9ec087fe136a7058917154747590f5eabda8
Yunus Emre Yıldırım <yunusemr4365@icloud.com>
2025-09-12 23:56:10 +0300

    Initial Commit

---
+++ HabitForge/HabitForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitForgeApp.swift
+//  HabitForge
+//
+//  Created by yunus emre yıldırım on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitForgeTests/HabitForgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitForgeTests.swift
+//  HabitForgeTests
+//
+//  Created by yunus emre yıldırım on 12.09.2025.
+//
+
+import Testing
+@testable import HabitForge
+
+struct HabitForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
